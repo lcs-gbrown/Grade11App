1c7e5cb8b2cd5566f01790ea19c5ac660584301c
gabi brown <gbrown@lcs.on.ca>
2021-11-24 11:31:18 -0500

    Initial Commit

---
+++ LCSFeedback/LCSFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSFeedbackApp.swift
+//  LCSFeedback
+//
+//  Created by gabi brown on 2021-11-24.
+//
+
+import SwiftUI
+
+@main
+struct LCSFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
